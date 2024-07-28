99bd1bb77e827f0203d8fc103db0417231836a42
ehsan2793 <ehsan2793@gmail.com>
2024-07-28 03:36:02 -0700

    Initial Commit

---
+++ weatherApp/weatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAppApp.swift
+//  weatherApp
+//
+//  Created by Ehsan Rahimi on 7/28/24.
+//
+
+import SwiftUI
+
+@main
+struct weatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
